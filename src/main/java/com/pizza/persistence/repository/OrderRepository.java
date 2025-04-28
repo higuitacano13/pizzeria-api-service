54d252fb630b82d7312b791646f4684a7079b1a1
Juan Higuita <jhiguitac@gmail.com>
2025-04-27 22:11:06 -0500

    Se utilizan queries nativos

--- src/main/java/com/pizza/persistence/repository/OrderRepository.java
+++ src/main/java/com/pizza/persistence/repository/OrderRepository.java
@@ -1,7 +1,9 @@
 package com.pizza.persistence.repository;
 
 import com.pizza.persistence.entity.OrderEntity;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.ListCrudRepository;
+import org.springframework.data.repository.query.Param;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -9,4 +11,7 @@ import java.util.List;
 public interface OrderRepository extends ListCrudRepository<OrderEntity, Integer> {
     List<OrderEntity> findAllByDateAfter(LocalDateTime date);
     List<OrderEntity> findAllByMethodIn(List<String> methods);
+
+    @Query(value = "Select * From pizza_order Where id_customer = :id", nativeQuery = true)
+    List<OrderEntity> findCustomerOrders(@Param("id") String idCustomer);
 }
--- src/main/java/com/pizza/service/OrderService.java
+++ src/main/java/com/pizza/service/OrderService.java
@@ -37,5 +37,9 @@ public class OrderService {
         return this.orderRepository.findAllByMethodIn(methods);
     }
 
+    public List<OrderEntity> getCustomerOrders(String customerId) {
+        return this.orderRepository.findCustomerOrders(customerId);
+    }
+
 
 }
