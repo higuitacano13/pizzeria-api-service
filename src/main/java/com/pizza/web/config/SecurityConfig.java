f332fde84daf3a5ba7c41094ab71fc9a42066775
Juan Higuita <jhiguitac@gmail.com>
2025-05-06 22:49:39 -0500

    Se realiza configuración para CORS

--- src/main/java/com/pizza/web/config/SecurityConfig.java
+++ src/main/java/com/pizza/web/config/SecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -13,7 +14,9 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests(customizeRequests -> {
             customizeRequests.anyRequest().authenticated();
-        }).httpBasic(Customizer.withDefaults());
+        }).csrf(AbstractHttpConfigurer::disable)
+                .cors(Customizer.withDefaults())
+                .httpBasic(Customizer.withDefaults());
         return http.build();
     }
 
