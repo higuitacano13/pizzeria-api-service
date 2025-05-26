8f6922a826c66fdd458e2f2fffef802628f4e7f8
Juan Higuita <jhiguitac@gmail.com>
2025-05-26 17:32:36 -0500

    Se crea auditoria de usuario en la entidad pizzas

--- src/main/java/com/pizza/persistence/audit/AuditableEntity.java
+++ src/main/java/com/pizza/persistence/audit/AuditableEntity.java
@@ -2,7 +2,9 @@ package com.pizza.persistence.audit;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.MappedSuperclass;
+import org.springframework.data.annotation.CreatedBy;
 import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
 import org.springframework.data.annotation.LastModifiedDate;
 
 import java.time.LocalDateTime;
@@ -16,4 +18,12 @@ public class AuditableEntity {
     @Column(name = "update_date")
     @LastModifiedDate
     private LocalDateTime updateAt;
+
+    @Column(name = "created_by")
+    @CreatedBy
+    private String createBy;
+
+    @Column(name = "modify_by")
+    @LastModifiedBy
+    private String modifyBy;
 }
